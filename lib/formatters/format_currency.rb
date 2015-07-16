d93750e0746c1133c3f5dcad2b08616f7208527b
Fabio Vilela <fbvilela@gmail.com>
2015-07-16 11:42:26 +1000

    adapting this for idashboard. it was saving and displaying wrong

--- lib/formatters/format_currency.rb
+++ lib/formatters/format_currency.rb
@@ -2,22 +2,21 @@ require 'rubygems'
 require 'action_view'
 
 module Formatters
-	class FormatCurrency < Format
-		include ActionView::Helpers::NumberHelper
+  class FormatCurrency < Format
+    include ActionView::Helpers::NumberHelper
 
-		def initialize(options = {})
-			@precision = options[:precision] || 2
-		end
+    def initialize(options = {})
+      @precision = options[:precision] || 2
+    end
 
-		def from(value, options = {})
-		  options = {:precision => @precision}.merge(options)
-			number_to_currency(value.to_f / (10 ** @precision), options)
-		end
+    def from(value, options = {})
+      options = {:precision => @precision}.merge(options)
+      number_to_currency(value.to_d, options)
+    end
 
-		def to(str, options = {})
-			return nil if str.nil? or str.empty?
-			val = str.gsub(/[^0-9.\-]/, '').to_f
-			(val * (10 ** @precision)) unless val.nil?
-		end
-	end
+    def to(str, options = {})
+      return nil if str.nil? or str.empty?
+      str.gsub(/[^0-9.\-]/, '').to_d
+    end
+  end
 end
